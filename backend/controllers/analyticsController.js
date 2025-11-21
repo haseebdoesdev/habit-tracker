6629bf0eb1462fe84bf5273952a85c8eba3db5ed
haseebdoesdev <abdlhaseeb17@gmail.com>
2025-11-21 11:26:33 +0500

    feat: Add analytics controller to calculate user habit completion rate and retrieve habit streaks.

--- backend/controllers/analyticsController.js
+++ backend/controllers/analyticsController.js
@@ -1,13 +1,31 @@
+const Habit = require('../models/Habit');
+const Log = require('../models/Log');
+
 exports.getAnalytics = async (req, res) => {
     try {
-        // TODO: Calculate completion rates
-        // WHY: Show user how well they are doing
+        const userId = req.user.id;
 
-        // TODO: Get streak data
-        // WHY: Motivate user with streak info
+        // Get all logs for the user
+        const logs = await Log.find({ user: userId });
+        const totalLogged = logs.length;
+        const totalCompleted = logs.filter(log => log.status === 'completed').length;
 
-        res.status(200).json({ message: 'Get analytics - to be implemented' });
+        let completionRate = 0;
+        if (totalLogged > 0) {
+            completionRate = (totalCompleted / totalLogged) * 100;
+        }
+
+        // Get all habits for the user to show streaks
+        const habits = await Habit.find({ user: userId }).select('title streak');
+
+        res.status(200).json({
+            completionRate: completionRate.toFixed(2),
+            totalCompleted,
+            totalLogged,
+            habits
+        });
     } catch (error) {
+        console.error(error.message);
         res.status(500).json({ message: 'Server error' });
     }
 };
