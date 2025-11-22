e8541c3415e4e1435529490f9bf5fe1706382e4e
haseebdoesdev <abdlhaseeb17@gmail.com>
2025-11-22 11:44:49 +0500

    Revert "Merge pull request #1 from haseebdoesdev/feature-implement-login-jsx"
    This reverts commit d4c69f82a17f2516422deef953fcb8bc66b303b1, reversing
    changes made to c6511c8a58c14f46e2700306a26be7300f0b8989.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Login from './components/login';
 // TODO: Import components (Login, Register, Dashboard, etc.)
 // WHY: Map routes to specific page components
 
@@ -12,7 +11,6 @@ const App = () => {
                 {/* WHY: Navigation should be visible on most pages */}
 
                 <Routes>
-                    <Route path="/login" element={<Login />} />
                     {/* TODO: Define Route for /login */}
                     {/* WHY: Public route for authentication */}
 
