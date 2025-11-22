9583d11b99612b09d4e535573019d39d05ff8e7b
haseebdoesdev <abdlhaseeb17@gmail.com>
2025-11-22 10:20:41 +0500

    feat: Add states for register componenet

--- frontend/src/components/Auth/Register.jsx
+++ frontend/src/components/Auth/Register.jsx
@@ -3,7 +3,9 @@ import React, { useState } from 'react';
 const Register = () => {
     // TODO: Define state for name, email, password
     // WHY: Collect new user information
-
+    const [name, setName] = useState('');
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
     // TODO: Define handleRegister function
     // WHY: Send registration data to backend
 
