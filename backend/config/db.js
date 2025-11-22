e5ba7b37490e86bfae35e16753293a9f48548583
haseebdoesdev <abdlhaseeb17@gmail.com>
2025-11-22 13:42:55 +0500

    feat: add initial MongoDB connection utility using Mongoose

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,14 +2,18 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
-    // TODO: Connect to MongoDB using mongoose.connect()
+    // Connect to MongoDB using mongoose.connect()
     // WHY: Establish connection to the database to store and retrieve data
     // HINT: Use process.env.MONGO_URI for the connection string
-    
+    await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+
     console.log('MongoDB Connected...');
   } catch (err) {
     console.error(err.message);
-    // TODO: Exit process with failure
+    // Exit process with failure
     // WHY: If DB connection fails, the server cannot function properly
     process.exit(1);
   }
